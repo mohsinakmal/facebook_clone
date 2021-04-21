e92cf8c563440a284e7fde8c439b4384f0b689e3
mohsin akmal <mohsinakmal210@gmail.com>
2021-04-21 16:50:44 +0500

    4/21/2021

---
+++ flutter_facebook_responsive_ui-starter-project/lib/screens/home_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import '../config/palette.dart';
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            brightness: Brightness.light,
+            backgroundColor: Colors.white,
+            title:
+            Text('facebook',
+              style: TextStyle(
+                color: Palette.facebookBlue,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: -1.2,
+              ),
+            ),
+            floating: true,
+            actions: [
+              Container(
+                margin: EdgeInsets.all(6.0),
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  shape: BoxShape.circle,
+                ),
+                child: IconButton(icon: Icon(Icons.search),
+                    iconSize: 30.0,
+                    color: Colors.black,
+                    onPressed: (){}),
+              ),
+              Container(
+                margin: EdgeInsets.all(6.0),
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  shape: BoxShape.circle,
+                ),
+                child: IconButton(icon: Icon(Icons.search),
+                    iconSize: 30.0,
+                    color: Colors.black,
+                    onPressed: (){}),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
